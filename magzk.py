d8a4d87dc109601ff2aa61ea35ea2236cf0c7a8c
mag7zk <160586341+mag7zk@users.noreply.github.com>
2024-03-17 11:31:57 +0100

    Update magzk.py

--- magzk.py
+++ magzk.py
@@ -7,9 +7,8 @@ print("v + u = ",x)
 z = complex(v,u);
 print ("The phase of complex number is : ",end="")
 print (cmath.phase(z))
-print (cmath.abs(z))
-print ("The rectangular form of complex number is : ",end="")
-print (w)
+print ("Sqrt(z): ",end="")
+print (cmath.sqrt(z))
 # using the exponentiation operator
 ex = v ** u
 #Modulus: returns the remainder when the first operand is divided by the second
